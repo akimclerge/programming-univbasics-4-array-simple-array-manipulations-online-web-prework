9866ed6b4861024a95a300d2864616b82610c2df
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-28 15:13:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,13 @@
 def using_push
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = ["violet"]
+    next_color = "violet"
     colors_in_the_rainbow.push(next_color)
   end
 def using_unshift
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    new_neighborhood = ["Staten Island"]
+ while  
+ bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
+    do
     bouroughs_in_nyc.unshift(new_neighborhood)
   end
 def using_pop
